f6ffdc147ceb6d93b5df9a23911f55d8917606da
ZippyToaster <seanbuddy9@gmail.com>
2025-03-03 10:28:58 -0800

    hiii

---
+++ js/event.js
@@ -0,0 +1,28 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+const element5 = document.getElementById('element5')
+
+element1.addEventListener('click', () => {
+    element1.innerHTML = "I've been clicked! <br> Try double clicking me."
+})
+
+element1.addEventListener('dblclick', () => {
+    element1.innerHTML = "I've been double clicked! <br> Try single clicking me."
+})
+
+element2.addEventListener('mouseover', () => {
+    element2.style.color = "green"
+    element2.style.backgroundColor = "blue"
+})
+
+element2.addEventListener('mouseout', () => {
+    element2.style.color = "white"
+    element2.style.backgroundColor = "#BB33BB"
+})
+
+element3.addEventListener('mouseout', () => {
+    element2.style.color = "white"
+    element2.style.backgroundColor = "#BB33BB"
+})
