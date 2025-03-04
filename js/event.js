5bc38d930680a90c88e8795844e8d0ad3e941a2f
ZippyToaster <seanbuddy9@gmail.com>
2025-03-04 17:50:08 +0000

    Tue, Mar 4, 2025, 9:50 AM -08:00

--- js/event.js
+++ js/event.js
@@ -22,6 +22,13 @@ element2.addEventListener('mouseout', () => {
     element2.style.backgroundColor = "#BB33BB"
 })
 
+let positionY = 0;
+let positionX = 0;
+
 document.addEventListener('keyup', (event) => {
     console.log(event.key)
+
+    if (event.key == 'ArrowDown') {
+
+    }
 })
