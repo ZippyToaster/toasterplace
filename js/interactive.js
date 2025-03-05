859b043e45683682fdd79a0f44fde9e644d1b450
ZippyToaster <seanbuddy9@gmail.com>
2025-03-05 17:53:47 +0000

    Wed, Mar 5, 2025, 9:53 AM -08:00

--- js/interactive.js
+++ js/interactive.js
@@ -4,10 +4,15 @@ function getRandomNumb(maxnumb) {
     return Math.floor(Math.random() * maxnumb);
 }
 
+let posX = 0;
+let poxY = 0;
+
 button1.addEventListener('click', () => {
     button1.classList.toggle('active')
-    button1.style.top = `${getRandomNumb(500)}px`
-    button1.style.right = `${getRandomNumb(1000)}px`
+    posX = getRandomNumb(1000)
+    posY = getRandomNumb(500)
+    button1.style.top = `${posY}px`
+    button1.style.right = `${posX}px`
     setTimeout(() => {
         button1.classList.toggle('active')
       }, 125)
