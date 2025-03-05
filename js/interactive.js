2adca43d0ee8c6eccf8ea74e71a6af6e17715b78
ZippyToaster <seanbuddy9@gmail.com>
2025-03-05 17:31:32 +0000

    Wed, Mar 5, 2025, 9:31 AM -08:00

--- js/interactive.js
+++ js/interactive.js
@@ -1,5 +1,10 @@
 const button1 = document.getElementById('button1')
 
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
 button1.addEventListener('click', () => {
     button1.classList.toggle('active')
+    console.log(getRandomNumber(10));
 })
