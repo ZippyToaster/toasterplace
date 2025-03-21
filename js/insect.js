0a41180fe13064d59502ddc42d47fb826573a272
ZippyToaster <seanbuddy9@gmail.com>
2025-03-21 16:34:11 +0000

    Fri, Mar 21, 2025, 9:34 AM -07:00

---
+++ js/insect.js
@@ -0,0 +1,64 @@
+const screens = document.querySelectorAll('.screen')
+const start_btn = document.getElementById('start-btn')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+
+let seconds = 1
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        screens[1].classList.add('up')
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = {src, alt}
+        startGame()
+        createInsect()
+    })
+})
+
+function startGame() {
+    setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+    let s = seconds % 60
+    let m = Math.floor(seconds / 60)
+    if (s < 10) {
+        s = `0${s}`
+    }
+    if (m < 10) {
+        m = `0${m}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds += 1
+    getRandomLocation()
+}
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}">`
+
+    game_container.appendChild(insect)
+}
+
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    console.log(Math.floor(Math.random(1, 10) * 10))
+    //const x = Math.random() *
+}
+
+// function getRandomNumb(maxnumb) {
+//     return Math.floor(Math.random() * maxnumb);
+// }
