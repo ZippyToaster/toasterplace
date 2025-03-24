5773a7650e5df0e32dd1399131b7d0add0c57f0f
ZippyToaster <seanbuddy9@gmail.com>
2025-03-24 17:04:34 +0000

    Mon, Mar 24, 2025, 10:04 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -75,7 +75,7 @@ function increaseScore() {
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
-    const x = Math.random() * width
+    const x = Math.random() * (width - 200px) + 100
     const y = Math.random() * height
     return {x, y}
 }
