ec72698b24c8dee058e8da4d6c1d6fe7c1f703d8
ZippyToaster <seanbuddy9@gmail.com>
2025-03-25 17:27:24 +0000

    Tue, Mar 25, 2025, 10:27 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const container = document.querySelector('.container')
+const panel = document.querySelector('.panel')
+
+container.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+    container.classList.add('active')
+})
+
+function removeActiveClasses(){
+     panel.classList.remove('active')
+     container.classList.remove('active')
+}
+
