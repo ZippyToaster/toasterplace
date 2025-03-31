0f2979a86d72315abb6f7b7f4eec9b809897fdf8
ZippyToaster <seanbuddy9@gmail.com>
2025-03-30 19:51:14 -0700

    fixes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 const container = document.querySelector('.container')
 const panel = document.querySelector('.panel')
+const arrow = document.getElementById('arrowbtn')
+const arrowimg = document.getElementById('arrowimg')
+const links = document.querySelectorAll('.link')
+const title = document.querySelector('.title')
+const description = document.querySelector('.description')
 
-container.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
-    container.classList.add('active')
+arrow.addEventListener('click', () => {
+    panel.classList.toggle('active')
+    container.classList.toggle('active')
+    arrow.classList.toggle('active')
+    title.classList.toggle('active')
+    description.classList.toggle('active')
+    links.forEach(link => {
+        link.classList.toggle('active')
+    })
 })
-
-function removeActiveClasses(){
-     panel.classList.remove('active')
-     container.classList.remove('active')
-}
 
